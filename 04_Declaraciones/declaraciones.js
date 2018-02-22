b9c909e66068c12d19b5c091358bd51a04cd98fc
msvzero <msvzero@gmail.com>
2018-02-22 01:27:58 -0300

    Declaraciones y Variables

---
+++ 04_Declaraciones/declaraciones.js
@@ -0,0 +1,10 @@
+// Declaraciones, instrucciones o statement
+var a, b, c, d; // Se declaran  variables
+a = 5; // Asiganacion de valor
+b = 6; // Asiganacion de valor
+//JavaScript ignora los espacios en blanco
+c=a + b; // Asiganacion de valor
+// d = c*2;
+
+/**break:	Terminates a switch or a loop
+continue:	Jumps out of a loop and starts at the top */
---
+++ 05_Variables/index.js
@@ -0,0 +1,21 @@
+
+var a = 10; //Number
+var nombre = "Martin"; // String
+
+// JavaScript es case sensitive e decir distingue mayusculas y minisculas.
+var Nombre = "Juan Perez"
+var $apellido = "Serratti"
+
+// var var = "hola"
+
+var _titulo = "Programando en JavaScript";
+var b = 10.5; 
+var llueve = false; // boolean
+
+var articulo = { //objeto
+    titulo: "Programando con javascript",
+    autor: "Martin Serratti",
+    texto: "Java no es lo mismo que javascript"
+}
+
+console.log(llueve);
