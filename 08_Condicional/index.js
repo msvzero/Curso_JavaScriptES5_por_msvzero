b2940154c1ea38cfaba33a9db0a9ff76c988976a
msvzero <msvzero@gmail.com>
2018-02-27 01:03:47 -0300

    Condicional 'if'

---
+++ 08_Condicional/index.js
@@ -0,0 +1,34 @@
+var a = 106;
+var b = 105;
+//Sintaxis basica
+if(a > b){
+    console.log("El valor de 'a' es mayor que el valor de 'b'");
+}
+
+// Else
+/*if (a > b) {
+    console.log("El valor de 'a' es mayor que el valor de 'b'");
+}else {
+    console.log("El valor de 'b' es mayor que el valor de 'a'");
+}*/
+
+// Multiples condiciones
+/*if(a > b){
+    console.log("El valor de 'a' es mayor que el valor de 'b'");
+} else if(a < 25) {
+    console.log("El valor de 'a' es menor a 25 ");
+} else if(a > 7) {
+    console.log("El valor de 'a' es mayor a 7 ");
+} else{
+    console.log("El valor de 'a' es: ", a);
+}*/
+
+// AND y OR
+/*if( a > b && a > 110){
+    console.log("El valor de 'a' es mayor al valor de 'b' y mayor a 100 ");
+}*/
+
+/*if( a > b || a > 110){
+    console.log("El valor de 'a' es: ", a);
+}*/
+
