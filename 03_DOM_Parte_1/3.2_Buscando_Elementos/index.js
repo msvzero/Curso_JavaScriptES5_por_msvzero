d7dd72af308b1e68aead2ed2b878b2d7e4ee5bbb
mszero <martinstti@gmail.com>
2018-03-04 23:18:12 -0300

    Trabajando con el DOM primera parte

---
+++ 03_DOM_Parte_1/3.2_Buscando_Elementos/index.js
@@ -0,0 +1,15 @@
+//Buscamos elementos por el id
+//Buscamos el elemento "tituloPagina"
+var tituloPagina = document.getElementById("tituloPagina");
+console.log("Titulo de la pagina ",tituloPagina);
+
+//Buscando elementos por el nombre de la clase.
+//Buscamos los encabezados de cada articulo
+var encabezados = document.getElementsByClassName("encabezado");
+console.log("Encabezados: ", encabezados);
+
+//Buscamos elementos por el nombre del tag
+//Buscamos los elementos "p"
+var parrafos = document.getElementsByTagName("p");
+console.log(parrafos);
+
