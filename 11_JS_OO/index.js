5f12b6f46ba053f1a46f766a07e68321a3278531
mszero <martinstti@gmail.com>
2018-03-07 15:28:24 -0300

    Objetos en javascript

---
+++ 11_JS_OO/index.js
@@ -0,0 +1,12 @@
+/*Objetos en JavaScript
+    - Atributos
+    - Metodos
+*/
+//Object Literal
+var persona = {nombre:"Juan", apellido: "Perez", edad:23, nombreCompleto: function(){ return this.nombre+" "+this.apellido}};
+
+console.log("Datos: ",persona);
+console.log("Nombre: ", persona.nombre);
+console.log("Nombre: ", persona["nombre"]);
+console.log("Nombre completo: ", persona.nombreCompleto());
+console.log("Nombre completo: ", persona.nombreCompleto().toLocaleUpperCase());
