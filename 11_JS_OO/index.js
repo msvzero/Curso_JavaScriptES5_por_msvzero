a19cebf07b00ee8a5e76d17a76754045bb3c7616
msvzero <msvzero@gmail.com>
2018-03-08 15:11:16 -0300

    for in loop en objeto

--- 11_JS_OO/index.js
+++ 11_JS_OO/index.js
@@ -5,8 +5,12 @@
 //Object Literal
 var persona = {nombre:"Juan", apellido: "Perez", edad:23, nombreCompleto: function(){ return this.nombre+" "+this.apellido}};
 
-console.log("Datos: ",persona);
+/*console.log("Datos: ",persona);
 console.log("Nombre: ", persona.nombre);
 console.log("Nombre: ", persona["nombre"]);
 console.log("Nombre completo: ", persona.nombreCompleto());
-console.log("Nombre completo: ", persona.nombreCompleto().toLocaleUpperCase());
+console.log("Nombre completo: ", persona.nombreCompleto().toLocaleUpperCase());*/
+
+for (var atributo in persona){
+    console.log(persona[atributo]);
+}
