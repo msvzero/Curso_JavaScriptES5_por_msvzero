337fc468131e7e2dc8a9248c6ed726bff9b6064b
mszero <martinstti@gmail.com>
2018-03-09 14:20:28 -0300

    DOM actualizado

--- 03_DOM_Basico/3.2_Buscando_Elementos/index.js
+++ 03_DOM_Basico/3.2_Buscando_Elementos/index.js
@@ -1,15 +1,33 @@
 //Buscamos elementos por el id
 //Buscamos el elemento "tituloPagina"
 var tituloPagina = document.getElementById("tituloPagina");
-console.log("Titulo de la pagina ",tituloPagina);
+console.log("Por id ",tituloPagina);
 
 //Buscando elementos por el nombre de la clase.
 //Buscamos los encabezados de cada articulo
 var encabezados = document.getElementsByClassName("encabezado");
-console.log("Encabezados: ", encabezados);
+console.log("Por clases: ", encabezados);
 
 //Buscamos elementos por el nombre del tag
 //Buscamos los elementos "p"
 var parrafos = document.getElementsByTagName("p");
-console.log(parrafos);
+console.log("Por tag",parrafos);
+
+//Bucamos un elemento por el selector CSS
+//var elemento = document.querySelectorAll("h1.tituloArticulo"); //The querySelectorAll() method does not work in Internet Explorer 8 and earlier versions.
+//console.log("Por selector", elemento);
+
+
+var elemento = document.getElementById("contenedor");
+console.log(elemento.children);
+console.log(elemento.children[0]);
+console.log(elemento.children[0].innerHTML);
+console.log(elemento.firstElementChild);
+
+var segundoPost = elemento.children[2].children[1];
+console.log(segundoPost);
+console.log(segundoPost.children[0])
+
+
+
 
