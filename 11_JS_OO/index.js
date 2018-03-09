6efee2672e365e3bf04c888deb8fb4d6e9100a7e
mszero <martinstti@gmail.com>
2018-03-09 17:07:33 -0300

    Actualizacion JS_ OO Y DOM_Basico

--- 11_JS_OO/index.js
+++ 11_JS_OO/index.js
@@ -14,3 +14,54 @@ console.log("Nombre completo: ", persona.nombreCompleto().toLocaleUpperCase());*
 for (var atributo in persona){
     console.log(persona[atributo]);
 }
+
+
+var posts = 
+[
+    {
+        titulo: "Programacion en javascript",
+        texto: "javaScript is the programming language of HTML and the Web",
+    },
+    {
+        titulo: "Intro a Linux",
+        texto: "javaScript is the programming language of HTML and the Web",
+    },
+    {
+        titulo: "No usen Windows pirata",
+        texto: "javaScript is the programming language of HTML and the Web",
+    },
+    
+
+]
+
+//console.log(posts);
+
+/*for(var i = 0; i <= posts.length; i++){
+    for(atributo in posts[i]){
+        console.log(posts[i][atributo]);
+    }
+}*/
+
+posts.map(function(post){
+    //console.log(post.titulo);
+    var titulo = post.titulo;
+    var texto = post.texto;
+
+    var contenedor = document.getElementById("contenedor");
+
+    var header = document.createElement("H2");
+    var textoTitulo = document.createTextNode(titulo);
+
+    var parrafo = document.createElement("P");
+    var textoParrafo = document.createTextNode(texto);
+
+
+
+   // console.log(tituloPost)
+    header.appendChild(textoTitulo);
+    parrafo.appendChild(textoParrafo);
+
+    contenedor.appendChild(header);
+    contenedor.appendChild(parrafo);
+
+})
